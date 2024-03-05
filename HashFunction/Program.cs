28ac62526729d129887f0cf24146b4f7ecba5ce4
dileep <dileep20721@gmail.com>
2024-03-05 18:19:36 +0530

    Add project files.

---
+++ HashFunction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace HashFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "Hello World";
+            string hashCode=ComputeSHA256Hash(input);
+            Console.WriteLine(input);
+            Console.WriteLine(hashCode);
+        }
+
+        private static string ComputeSHA256Hash(string input)
+        {
+            using(SHA256 sha256 = SHA256.Create()) 
+            {
+                byte[] bytes=sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+                StringBuilder sb=new StringBuilder();
+                for(int i = 0; i < bytes.Length; i++) 
+                {
+                    sb.Append(bytes[i].ToString());
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
